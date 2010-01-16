5ec844c1d806245fc56c7369bbf5f3a5110520fd
mollifier <mollifier@gmail.com>
2010-01-17 00:13:17 +0900

    スクリプト本体 新規登録

---
+++ linkToMtg.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name linkToMtg
+// @namespace http://d.hatena.ne.jp/mollifier/
+// @description Add link to MTG gatherer
+// @include http://mtgwiki.com/wiki/*
+// ==/UserScript==
+//
+// auther : mollifier http://d.hatena.ne.jp/mollifier/
+// version : 0.0.1
+//
+// linkToMtg is released under the MIT License.
+// http://www.opensource.org/licenses/mit-license.php
+
+(function() {
+  console.log("linkToMtg");
+})();
+
